b80ed5ae341dc9f4e94710d6ea79790abc469cda
mathlima1 <mathlima1@hotmail.com.br>
2021-09-08 21:20:26 -0300

    Validando e criando usuarios no Fauna DB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -10,4 +14,38 @@ export default NextAuth({
             scope: 'read:user'
         }),
     ],
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user
+
+            try {
+                await fauna.query(//vamos entrar no fauna
+                    q.If(//se
+                        q.Not(//não
+                            q.Exists(//existir
+                                q.Match(//um match(combinação) entre)
+                                    q.Index('user_by_email'),//o index user_by_email (criado la no faunadb)
+                                    q.Casefold(user.email)//e o email que o usuario está tentando logar
+                                )
+                            )
+                        ),
+                        q.Create(//se não existir um match entre aqueles dois, ele vai criar um novo usuario 
+                            q.Collection('user'),
+                            { data: { email } }
+                        ),
+                        q.Get(// caso exista ele so vai dar Get (pegar) as informações desse mesmo match
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+                return true
+            } catch {
+                return false
+            }
+
+        },
+    }
 })
