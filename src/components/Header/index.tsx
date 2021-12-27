e5bf44f2367488c292fd3d9ec1b9d915275f8657
mathlima1 <mathlima1@hotmail.com.br>
2021-12-27 15:10:24 -0300

    Alterando ancoras para Link do next

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import { SingInButton } from '../SingInButton'
 import styles from './styles.module.scss'
 
@@ -8,8 +10,12 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/assets/ig.news.svg" alt="Logo Ig.news" />
                 <nav>
-                    <a href="/" className={styles.active}>Home</a>
-                    <a href="/posts">Posts</a>
+                    <Link href="/">
+                        <a className={styles.active}>Home</a>
+                    </Link>
+                    <Link href="/posts" prefetch>
+                        <a >Posts</a>
+                    </Link>
                 </nav>
                 <SingInButton />
             </div>
