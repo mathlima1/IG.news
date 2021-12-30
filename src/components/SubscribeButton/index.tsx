5e1381f788cb162c04ff3eba6009ed92a4102298
mathlima1 <mathlima1@hotmail.com.br>
2021-12-29 22:19:47 -0300

    validando Inscrição ativa do usuario

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,8 @@ import styles from './styles.module.scss';
 import { useSession, signIn } from 'next-auth/client'
 import { api } from '../../services/api'
 import { getStripeJs } from '../../services/stripe-js';
+import { redirect } from 'next/dist/next-server/server/api-utils';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
     priceId: string;
@@ -9,9 +11,14 @@ interface SubscribeButtonProps {
 
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const [session] = useSession();
+    const router = useRouter()
     async function handleSubscribe() {
         if (!session) {
             signIn('github');
+            return;
+        }
+        if (session.activeSubscription) {
+            router.push('/posts')
             return;
         }
 
